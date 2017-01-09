ea0b880d53c77d1f3bab2f79ddfb4f21fac2e091
esuau <evan.suau@etu.u-pec.fr>
2017-01-09 21:52:57 +0100

    Fix ClassNotFoundException on MySQL driver when accessing database

--- src/main/java/fr/ladn/carsharingclub/ing1/db/PartDAO.java
+++ src/main/java/fr/ladn/carsharingclub/ing1/db/PartDAO.java
@@ -7,12 +7,12 @@ import java.sql.ResultSet;
 import fr.ladn.carsharingclub.ing1.model.Part;
 
 public class PartDAO {
-    public void create(Part part) throws Exception{
+    public void create(Part part) throws Exception {
         String reference = part.getReference();
         String provider = part.getProvider();
         int availableQuantity = part.getAvailableQuantity();
-        float price  = part.getPrice();
-        
+        float price = part.getPrice();
+
         PreparedStatement ps = ConnectionDB.getConnection().prepareStatement("INSERT INTO pieces ( libelle_piece, fabricant, qte_dispo, valeur_piece ) VALUES ( ?, ?, ?, ? )");
         ps.setString(1, reference);
         ps.setString(2, provider);
@@ -20,27 +20,27 @@ public class PartDAO {
         ps.setFloat(4, price);
         ps.execute();
     }
-    
-    public Part read(int id) throws Exception{
+
+    public Part read(int id) throws Exception {
         PreparedStatement ps = ConnectionDB.getConnection().prepareStatement("SELECT * FROM pieces WHERE id_piece = ?");
         ps.setInt(1, id);
         ResultSet rs = ps.executeQuery();
-        if(rs.next()){
-            
-            String reference = rs.getString("reference");
-            String provider = rs.getString("provider");
-            int availableQuantity = rs.getInt("available quantity");
-            float price = rs.getFloat("price");
-            
+        if (rs.next()) {
+
+            String reference = rs.getString("libelle_piece");
+            String provider = rs.getString("fabricant");
+            int availableQuantity = rs.getInt("qte_dispo");
+            float price = rs.getFloat("valeur_piece");
+
             return new Part(id, reference, provider, availableQuantity, price);
-            
-        }else{
+
+        } else {
             return null;
         }
-        
+
     }
-    
-    public void update(Part part) throws Exception{
+
+    public void update(Part part) throws Exception {
         PreparedStatement ps = ConnectionDB.getConnection().prepareStatement("UPDATE pieces SET libelle_piece = ?, fabricant = ?, qte_dispo = ?, valeur_piece = ? WHERE id_piece = ?");
         ps.setString(1, part.getReference());
         ps.setString(2, part.getProvider());
@@ -49,11 +49,12 @@ public class PartDAO {
         ps.setInt(5, part.getId());
         ps.execute();
     }
-    public void delete(Part part) throws Exception{
+
+    public void delete(Part part) throws Exception {
         PreparedStatement ps = ConnectionDB.getConnection().prepareStatement("DELETE FROM pieces WHERE id_piece = ?");
         ps.setInt(1, part.getId());
         ps.execute();
     }
-    
-    
+
+
 }
