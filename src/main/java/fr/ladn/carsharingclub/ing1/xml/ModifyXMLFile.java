c8197a15b05a102c254ac5c0186ce125309c1efc
noel.diril <noeldiril@MacBook-Air-de-Noel.local>
2017-02-13 15:41:30 +0100

    Modification lecture / Ajout modifiacation

---
+++ src/main/java/fr/ladn/carsharingclub/ing1/xml/ModifyXMLFile.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+
+public class ModifyXMLFile {
+	public static void main(final String[] args) {
+        /*
+         * Etape 1 : récupération d'une instance de la classe "DocumentBuilderFactory"
+         */
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            	
+        try {
+            /*
+             * Etape 2 : création d'un parseur
+             */
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+			
+	    /*
+	     * Etape 3 : création d'un Document
+	     */
+	    final Document document= builder.parse(new File("repertoire.xml"));	    /*
+	     * Etape 4 : récupération de l'Element racine
+	     */
+	    final Element racine = document.getDocumentElement();
+	    /*
+	     * Etape 5 : récupération des pieces
+	     */
+	    //final NodeList racineNoeuds = racine.getChildNodes();
+	    
+	   	final Element voiture = document.createElement("voiture");
+	   	voiture.setAttribute("numSerie", "1234567890");
+	   	racine.appendChild(voiture);
+	   	final Element marque = document.createElement("marque");
+		final Element année = document.createElement("année");
+		final Element prix = document.createElement("prix");
+		
+		marque.appendChild(document.createTextNode("Mercedes"));
+		année.appendChild(document.createTextNode("2016"));
+		prix.appendChild(document.createTextNode("125000"));
+		
+		voiture.appendChild(marque);
+		voiture.appendChild(année);
+		voiture.appendChild(prix);
+		
+		final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    final Transformer transformer = transformerFactory.newTransformer();
+	    final DOMSource source = new DOMSource(document);
+	    final StreamResult sortie = new StreamResult(new File("repertoire.xml"));
+	    
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	    transformer.transform(source, sortie);
+
+        }
+        catch (final ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        catch (final SAXException e) {
+            e.printStackTrace();
+        }
+        catch (final IOException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+    }
+
+//	   On enregistre notre nouvelle arborescence dans le fichier
+//	   d'origine dans un format classique.
+}
--- src/main/java/fr/ladn/carsharingclub/ing1/xml/ReadXMLFile.java
+++ src/main/java/fr/ladn/carsharingclub/ing1/xml/ReadXMLFile.java
@@ -1,97 +1,55 @@
-package fr.ladn.carsharingclub.ing1.xml;
+package com.mkyong.seo;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
+import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 
-public class ReadXMLFile {
-    public static void main(final String[] args) {
-        /*
-         * Etape 1 : récupération d'une instance de la classe "DocumentBuilderFactory"
-         */
-        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            	
-        try {
-            /*
-             * Etape 2 : création d'un parseur
-             */
-            final DocumentBuilder builder = factory.newDocumentBuilder();
-			
-	    /*
-	     * Etape 3 : création d'un Document
-	     */
-	    final Document document= builder.parse(new File("src/resources/repertoire.xml"));
-			
-	    //Affiche du prologue
-	    System.out.println("*************PROLOGUE************");
-	    System.out.println("version : " + document.getXmlVersion());
-	    System.out.println("encodage : " + document.getXmlEncoding());		
-            System.out.println("standalone : " + document.getXmlStandalone());
-					
-	    /*
-	     * Etape 4 : récupération de l'Element racine
-	     */
-	    final Element racine = document.getDocumentElement();
-		
-	    //Affichage de l'élément racine
-	    System.out.println("\n*************RACINE************");
-	    System.out.println(racine.getNodeName());
-		
-	    /*
-	     * Etape 5 : récupération des pieces
-	     */
-	    final NodeList racineNoeuds = racine.getChildNodes();
-	    final int nbRacineNoeuds = racineNoeuds.getLength();
-			
-	    for (int i = 0; i<nbRacineNoeuds; i++) {
-	        if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
-	        	final Element piece = (Element) racineNoeuds.item(i);
-				
-		    //Affichage d'une piece
-		    System.out.println("\n*************PIECE************");
-		    System.out.println("id : " + piece.getAttribute("id"));
-			
-	    	    /*
-		     * Etape 6 : récupération du libelle_piece et du prénom
-		     */
-		    final Element libelle_piece = (Element) piece.getElementsByTagName("libelle_piece").item(0);
-		    final Element fabricant = (Element) piece.getElementsByTagName("fabricant").item(0);
-					
-		    //Affichage du libelle_piece et du prénom
-		    System.out.println("libelle_piece : " + libelle_piece.getTextContent());
-		    System.out.println("fabricant : " + fabricant.getTextContent());
-					
-		    /*
-		     * Etape 7 : récupération de la quantité dispo et de la valeur
-		     */
-		    
-		    final Element qte_dispo = (Element) piece.getElementsByTagName("qte_dispo").item(0);
-		    final Element valeur_piece = (Element) piece.getElementsByTagName("valeur_piece").item(0);
-		    
-		    //Affichage du qte_dispo et du prénom
-		    System.out.println("qte_dispo : " + qte_dispo.getTextContent());
-		    System.out.println("valeur_piece : " + valeur_piece.getTextContent());
-		    
-	        }				
-	    }			
-        }
-        catch (final ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        catch (final SAXException e) {
-            e.printStackTrace();
-        }
-        catch (final IOException e) {
-            e.printStackTrace();
-        }		
+public class ReadXMLFile2 {
+
+  public static void main(String argv[]) {
+
+    try {
+
+	File fXmlFile = new File("repertoire.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+
+	//optional, but recommended
+	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	doc.getDocumentElement().normalize();
+
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+	NodeList nList = doc.getElementsByTagName("piece");
+
+	System.out.println("----------------------------");
+
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+
+		Node nNode = nList.item(temp);
+
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nNode;
+
+			System.out.println("id : " + eElement.getAttribute("id"));
+			System.out.println("libelle_piece : " + eElement.getElementsByTagName("libelle_piece").item(0).getTextContent());
+			System.out.println("fabricant : " + eElement.getElementsByTagName("fabricant").item(0).getTextContent());
+			System.out.println("qte_dispo : " + eElement.getElementsByTagName("qte_dispo").item(0).getTextContent());
+			System.out.println("valeur_piece : " + eElement.getElementsByTagName("valeur_piece").item(0).getTextContent());
+
+		}
+	}
+    } catch (Exception e) {
+	e.printStackTrace();
     }
+  }
+
 }
