075e6d3332d8ada40c05f90be46a6fe2d9c8168d
evan.suau <evan.suau@etu.u-pec.fr>
2017-02-14 09:22:28 +0100

    Add missing Part class

---
+++ src/main/java/fr/ladn/carsharingclub/ing1/model/Part.java
@@ -0,0 +1,104 @@
+package fr.ladn.carsharingclub.ing1.model;
+
+/**
+ * Part business object
+ */
+public class Part {
+
+    private int id;
+    private String reference;
+    private String provider;
+    private int availableQuantity;
+    private float price;
+
+    /**
+     * Default constructor
+     *
+     * @param id                of the part
+     * @param reference         (name) of the part
+     * @param provider          or supplier of the part
+     * @param availableQuantity of this part reference in the stock
+     * @param price             (cost) of the part
+     */
+    public Part(int id, String reference, String provider, int availableQuantity, float price) {
+        this.id = id;
+        this.reference = reference;
+        this.provider = provider;
+        this.availableQuantity = availableQuantity;
+        this.price = price;
+    }
+
+    /**
+     * Custom constructor (ID is not defined)
+     *
+     * @param reference         (name) of the part
+     * @param provider          or supplier of the part
+     * @param availableQuantity of this part reference in the stock
+     * @param price             or cost of the part
+     */
+    public Part(String reference, String provider, int availableQuantity, float price) {
+        this.reference = reference;
+        this.provider = provider;
+        this.availableQuantity = availableQuantity;
+        this.price = price;
+    }
+
+    /**
+     * Gets part ID.
+     *
+     * @return id of a part
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Gets part name.
+     *
+     * @return reference of a part
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     * Gets part provider or supplier.
+     *
+     * @return the provider name of a part
+     */
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    /**
+     * Gets part cost or price.
+     *
+     * @return cost of a part
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * Gets the available quantity of a part.
+     *
+     * @return the number of available items for a given part
+     */
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+
+}
--- src/main/java/fr/ladn/carsharingclub/ing1/xml/ReadXMLFile.java
+++ src/main/java/fr/ladn/carsharingclub/ing1/xml/ReadXMLFile.java
@@ -1,6 +1,6 @@
+package fr.ladn.carsharingclub.ing1.xml;
 
 import java.io.StringReader;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -10,37 +10,36 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import fr.ladn.carsharingclub.ing1.model.Part;
+
 public class ReadXMLFile {
 
-  public Part parserXML(String xml) {
-	  
+    public Part parserXML(String xml) {
+        try {
+            //String xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?><piece id=\"11402907\"><libelle_piece>Pneu</libelle_piece><fabricant>Michelin</fabricant><qte_dispo >235</qte_dispo><valeur_piece >60</valeur_piece></piece>";
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(new InputSource(new StringReader(xml)));
 
-    try {
-    //String xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?><piece id=\"11402907\"><libelle_piece>Pneu</libelle_piece><fabricant>Michelin</fabricant><qte_dispo >235</qte_dispo><valeur_piece >60</valeur_piece></piece>";
-	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	Document doc = dBuilder.parse(new InputSource(new StringReader(xml)));
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("piece");
 
-	doc.getDocumentElement().normalize();
-	NodeList nList = doc.getElementsByTagName("piece");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
 
-	for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
 
-		Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-			Element eElement = (Element) nNode;
-
-			Part p = new Part(Integer.parseInt(eElement.getAttribute("id")),eElement.getElementsByTagName("libelle_piece").item(0).getTextContent(),eElement.getElementsByTagName("fabricant").item(0).getTextContent(),Integer.parseInt(eElement.getElementsByTagName("qte_dispo").item(0).getTextContent()),Float.valueOf(eElement.getElementsByTagName("valeur_piece").item(0).getTextContent()));
-			System.out.println("La piece a bien été retournée \n");
-			return p; 
-		}
-	}
-    } catch (Exception e) {
-	e.printStackTrace();
+                    Element eElement = (Element) nNode;
+                    Part p = new Part(Integer.parseInt(eElement.getAttribute("id")), eElement.getElementsByTagName("libelle_piece").item(0).getTextContent(), eElement.getElementsByTagName("fabricant").item(0).getTextContent(), Integer.parseInt(eElement.getElementsByTagName("qte_dispo").item(0).getTextContent()), Float.valueOf(eElement.getElementsByTagName("valeur_piece").item(0).getTextContent()));
+                    System.out.println("La piece a bien été retournée \n");
+                    return p;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-   return null;
-  }
- 
+
 }
