50f017d45fa1456f1c6c59d99bc7b8a735ac9e6d
evan.suau@etu.u-pec.fr <louis.endelicher@etu.u-pec.fr>
2017-06-09 15:50:19 +0200

    Implement OperationSorting modes

--- server/src/main/java/fr/ladn/carsharingclub/ing1/model/OperationList.java
+++ server/src/main/java/fr/ladn/carsharingclub/ing1/model/OperationList.java
@@ -9,53 +9,134 @@ import java.util.TreeSet;
  */
 public class OperationList implements Serializable {
 
-    /** The list of the operations. */
+    /**
+     * The sorting mode.
+     * A null sorting mode triggers the default sorting method.
+     */
+    private OperationSortingMode mode;
+
+    /**
+     * The list of operations.
+     */
     private SortedSet<Operation> elements = new TreeSet<Operation>() {
+
+        /**
+         * The sorting method.
+         *
+         * @param o1 an operation.
+         * @param o2 an other operation.
+         * @return the comparator value, negative if less, positive if greater.
+         */
         public int compare(Operation o1, Operation o2) {
-
-            // Comparison by failure type.
-            if (o1.getBiggestFailure().getType().equals(o2.getBiggestFailure().getType())) {
-
-                // Comparison by priority level.
-                if (o1.getPriority().equals(o2.getPriority())) {
-
-                    // Comparison by estimated time of the operation.
-                    if (o1.getTotalEstimatedDuration().equals(o2.getTotalEstimatedDuration())) {
-
-                        // Comparison by entry date of the vehicle.
-                        return o1.getDateEntry().compareTo(o2.getDateEntry());
-                    } else {
-                        return o1.getTotalEstimatedDuration().compareTo(o2.getTotalEstimatedDuration());
+            switch (mode) {
+                case FAILURE_TYPE:
+                    return compareFailureTypes(o1, o2);
+                case PRIORITY_LEVEL:
+                    return comparePriorityLevels(o1, o2);
+                case ESTIMATED_TIME:
+                    return compareEstimatedTimes(o1, o2);
+                case ENTRY_DATE:
+                    return compareEntryDates(o2, o1);
+                default:
+                    // The default sorting method.
+                    if (compareFailureTypes(o1, o2) == 0) {
+                        if (comparePriorityLevels(o1, o2) == 0) {
+                            if (compareEstimatedTimes(o1, o2) == 0) {
+                                return compareEntryDates(o2, o1);
+                            }
+                            return compareEstimatedTimes(o1, o2);
+                        }
+                        return comparePriorityLevels(o1, o2);
                     }
-                } else {
-                    return Integer.compare(o1.getPriority().getPriorityLevel(), o2.getPriority().getPriorityLevel());
-                }
-            } else {
-                return Integer.compare(o1.getBiggestFailure().getType().getPriority(), o2.getBiggestFailure().getType().getPriority());
+                    return compareFailureTypes(o1, o2);
             }
         }
+
     };
 
     /**
      * Default constructor.
      * Allows serialization.
      */
-    OperationList() { }
+    public OperationList() {
+
+    }
 
     /**
-     * Initializes the list of operations.
+     * Initializes the operation list with default sorting method.
      *
-     * @param elements the elements list.
+     * @param elements list of operations.
      */
     public OperationList(SortedSet<Operation> elements) {
         this.elements = elements;
     }
 
-    public SortedSet<Operation> getElements() {
+    /**
+     * Initializes the list of operations.
+     * Activates all sorting criteria.
+     *
+     * @param elements list of operations.
+     */
+    public OperationList(OperationSortingMode mode, SortedSet<Operation> elements) {
+        this.mode = mode;
+        this.elements = elements;
+    }
+
+    /**
+     * Compares the most important failures of two operations.
+     *
+     * @param o1 an operation.
+     * @param o2 the other operation to compare.
+     * @return the comparator value, negative if less, positive if greater.
+     */
+    private int compareFailureTypes(Operation o1, Operation o2) {
+        return Integer.compare(o1.getBiggestFailure().getType().getPriority(), o2.getBiggestFailure().getType().getPriority());
+    }
+
+    /**
+     * Compares by priority level.
+     *
+     * @param o1 an operation.
+     * @param o2 the other operation to compare.
+     * @return the comparator value, negative if less, positive if greater.
+     */
+    private int comparePriorityLevels(Operation o1, Operation o2) {
+        return Integer.compare(o1.getPriority().getPriorityLevel(), o2.getPriority().getPriorityLevel());
+    }
+
+    /**
+     * Compares the total estimated times of two operations.
+     *
+     * @param o1 an operation.
+     * @param o2 the other operation to compare.
+     * @return the comparator value, negative if less, positive if greater.
+     */
+    private int compareEstimatedTimes(Operation o1, Operation o2) {
+        return o1.getTotalEstimatedDuration().compareTo(o2.getTotalEstimatedDuration());
+    }
+
+    /**
+     * @param o1 an operation.
+     * @param o2 the other operation to compare.
+     * @return the comparator value, negative if less, positive if greater.
+     */
+    private int compareEntryDates(Operation o1, Operation o2) {
+        return o1.getDateEntry().compareTo(o2.getDateEntry());
+    }
+
+    public OperationSortingMode getMode() {
+        return mode;
+    }
+
+    public void setMode(OperationSortingMode mode) {
+        this.mode = mode;
+    }
+
+    public synchronized SortedSet<Operation> getElements() {
         return elements;
     }
 
-    public void setElements(SortedSet<Operation> elements) {
+    public synchronized void setElements(SortedSet<Operation> elements) {
         this.elements = elements;
     }
 
---
+++ server/src/main/java/fr/ladn/carsharingclub/ing1/model/OperationSortingMode.java
@@ -0,0 +1,27 @@
+package fr.ladn.carsharingclub.ing1.model;
+
+/**
+ * The custom sorting modes for the operation list.
+ */
+public enum OperationSortingMode {
+
+    /**
+     * Sorting by failure  type only.
+     */
+    FAILURE_TYPE,
+
+    /**
+     * Sorting by priority level only.
+     */
+    PRIORITY_LEVEL,
+
+    /**
+     * Sorting by total estimated time only.
+     */
+    ESTIMATED_TIME,
+
+    /**
+     * Sorting by vehicle entry date only.
+     */
+    ENTRY_DATE
+}
